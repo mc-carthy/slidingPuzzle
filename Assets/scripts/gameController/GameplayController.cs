131ea1d2a65cc9a623021b8024e2c7fc84ebc0a2
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-13 13:39:49 +0100

    Create menu controllers

---
+++ Assets/scripts/gameController/GameplayController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameplayController : MonoBehaviour {
+
+	public void BackToLevelSelect () {
+		SceneManager.LoadScene ("levelSelect", LoadSceneMode.Single);
+	}
+}
---
+++ Assets/scripts/gameController/LevelController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelController : MonoBehaviour {
+
+	public void SelectLevel () {
+
+		string[] name = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name.Split ();
+
+		int index = int.Parse (name [1]);
+
+		SceneManager.LoadScene ("main", LoadSceneMode.Single);
+	}
+
+	public void BackToMainMenu () {
+		SceneManager.LoadScene ("menu", LoadSceneMode.Single);
+	}
+}
