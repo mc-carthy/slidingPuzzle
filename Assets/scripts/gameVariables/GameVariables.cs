463d25eda3fe77b3bb89c967dd9e48ccfd6927de
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-13 13:49:13 +0100

    Create helper classes

---
+++ Assets/scripts/gameVariables/GameVariables.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class GameVariables {
+
+	public static int MaxRows = 5;
+	public static int MaxColumns = 5;
+	public static int MaxSize = MaxRows * MaxColumns;
+
+}
+
+public enum GameState {
+	Playing,
+	Animating,
+	End
+}
---
+++ Assets/scripts/puzzle/PizzlePiece.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PizzlePiece {
+
+
+	public int CurrentRow { get; set; }
+	public int CurrentColumn { get; set; }
+	public int OriginalRow { get; set; }
+	public int OriginalColumn { get; set; }
+	public GameObject GameObject { get; set; }
+
+}
